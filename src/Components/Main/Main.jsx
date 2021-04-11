0e75379450ec5aa8cd348e434e60a53b893a7a57
AmrHE <amr.hassan.emam@gmail.com>
2021-04-11 19:10:37 +0200

    update M-UI

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import useStyles from './style';;
+import useStyles from './style';
 
 
 const Main = () => {
@@ -11,6 +11,18 @@ const Main = () => {
       <CardHeader title="Expenses Tracker" subheader="Powered by Speechly" />
       <CardContent>
         <Typography align="center" variant="h5">Total Balance $100</Typography>
+        <Typography variant="subtitle1" style={{ LineHeight: '1.5em', marginTop: '20px' }}>
+          {/* InfoCard... */}
+          Try to say: Add income for $100 in category salary for Monday...
+        </Typography>
+        <Divider />
+        {/* Form */}
+      </CardContent>
+      
+      <CardContent className={classes.cardContent}>
+        <Grid>
+          
+        </Grid>
       </CardContent>
     </Card>
   )
