f83bbad157e911f03904ecb7fdb3e170c84d0777
AmrHE <amr.hassan.emam@gmail.com>
2021-04-14 21:20:37 +0200

    add Form Component

---
+++ src/Components/Main/Form/Form.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import useStyles from './FromStyle';
+
+const initialState = {
+  amount: '',
+  category: '',
+  type: 'Income',
+  date: new Date(),
+}
+
+const Form = () => {
+  const classes = useStyles();
+  const [formData, setFormData] = useState(initialState);
+  console.log(formData)
+
+  return (
+    <Grid container spacing={2}>
+
+      <Grid item xs={12}>
+        <Typography align="center" variant="subtitle2" gutterButtom>
+          {/* Here should contains what we get from Speechly */}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type:e.target.value })}>
+            <MenuItem value="Income">Income</MenuItem>
+            <MenuItem value="Expense">Expense</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Category</InputLabel>
+          <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
+          <MenuItem value="Business">Business</MenuItem>
+          <MenuItem value="Salary">Salary</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({...formData, amount: e.target.value})} />
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({...formData, date: e.target.value})} />
+      </Grid>
+
+      <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
+
+    </Grid>
+  )
+}
+
+export default Form
