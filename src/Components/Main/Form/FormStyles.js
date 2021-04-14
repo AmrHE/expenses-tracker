43dd235bc9bbbab2d47e84d0f149270a2f1466ca
AmrHE <amr.hassan.emam@gmail.com>
2021-04-14 21:23:14 +0200

    add From styles

---
+++ src/Components/Main/Form/FormStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles(() => ({
+  radioGroup: {
+    display: 'flex',
+    justifyCOntent: 'center',
+    marginButtom: '-10px',
+  },
+  button: {
+    marginTop: '20px'
+  },
+}));
