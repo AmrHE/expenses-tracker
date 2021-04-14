13d5293cd7d0f786ec0f2758aee80f0c97bcbfa9
AmrHE <amr.hassan.emam@gmail.com>
2021-04-14 21:24:46 +0200

    add List component

---
+++ src/Components/Main/List/List.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+import { ExpenseTrackerContext } from '../../../context/context';
+import useStyles from './listStyles';
+
+const List = () => {
+  const classes = useStyles();
+  const { deleteTransaction } = useContext(ExpenseTrackerContext);
+
+  const transactions = [
+    {
+      id: 1,
+      type: 'Income',
+      category: 'Salary',
+      amount: '1100',
+      date: "Wed Apr 14 2021",
+    },
+    {
+      id: 2,
+      type: 'Expense',
+      category: 'Business',
+      amount: '550',
+      date: "Wed Apr 14 2021",
+    },
+    {
+      id: 3,
+      type: 'Income',
+      category: 'Rent',
+      amount: '1100',
+      date: "Wed Apr 14 2021",
+    },
+  ];
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar 
+                className={
+                  transaction.type === 'Income' ? 
+                  classes.avatarIncome : 
+                  classes.avatarExpense 
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={`${transaction.category}`} secondary={`$${transaction.amount} - ${transaction.date}`}/>
+            <ListItemSecondaryAction>
+              <IconButton  edge="end" aria-label="delete" onClick="">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  )
+}
+
+export default List
