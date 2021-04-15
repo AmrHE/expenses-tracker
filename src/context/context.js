aae6d1cb905f90df23b9f95af79448d16b5b7ba6
AmrHE <amr.hassan.emam@gmail.com>
2021-04-15 22:17:45 +0200

    edit context

--- src/context/context.js
+++ src/context/context.js
@@ -12,15 +12,17 @@ export const Provider = ({ children }) => {
   const deleteTransaction = (id) => {
     dispatch({ type: 'DELETE_TRANSACTION', payload: id })
   };
-
+  
   const addTransaction = (transaction) => {
     dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
   };
+
   
   return (
     <ExpenseTrackerContext.Provider value={{ 
       deleteTransaction,
       addTransaction,
+      transactions
     }}>
       {children}
     </ExpenseTrackerContext.Provider>
