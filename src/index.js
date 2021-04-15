fd1f33a3f6de3e3aa564eccc624038dbda92f46a
AmrHE <amr.hassan.emam@gmail.com>
2021-04-15 22:08:26 +0200

    add context provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from './context/context';
+import './index.css';
 
-import './index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
+;
