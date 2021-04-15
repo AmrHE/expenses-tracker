bc5387a18be0a8110c2483d534c26ae8b49eec06
AmrHE <amr.hassan.emam@gmail.com>
2021-04-15 22:27:10 +0200

    edit import

--- src/Components/Main/Form/Form.jsx
+++ src/Components/Main/Form/Form.jsx
@@ -4,7 +4,7 @@ import { ExpenseTrackerContext } from '../../../context/context';
 import { v4 as uuidv4 } from 'uuid';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
 import formatDate from '../../../utils/formatDate';
-import useStyles from './FromStyle';
+import useStyles from './FormStyles';
 
 const initialState = {
   amount: '',
