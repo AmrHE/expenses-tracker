9c1218a3c300534f84f710cc67cca1e39e9f8520
AmrHE <amr.hassan.emam@gmail.com>
2021-04-15 22:12:28 +0200

    add List/Form Components

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import useStyles from './style';
+import useStyles from './mainStyle';
+import Form from './Form/Form';
+import List from './List/List';
 
 
 const Main = () => {
@@ -12,16 +14,18 @@ const Main = () => {
       <CardContent>
         <Typography align="center" variant="h5">Total Balance $100</Typography>
         <Typography variant="subtitle1" style={{ LineHeight: '1.5em', marginTop: '20px' }}>
-          {/* InfoCard... */}
-          Try to say: Add income for $100 in category salary for Monday...
+          {/* InfoCard Component... */}
+          Try saying: Add income for $100 in category salary for Monday...
         </Typography>
         <Divider />
-        {/* Form */}
+        <Form />
       </CardContent>
       
       <CardContent className={classes.cardContent}>
-        <Grid>
-          
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List/>
+          </Grid>
         </Grid>
       </CardContent>
     </Card>
