cadc6846d7814d31a3ab9cc084b4cda7fcf67649
AmrHE <amr.hassan.emam@gmail.com>
2021-04-17 22:31:38 +0200

    add speechlyProvider wrapper

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechlyProvider } from '@speechly/react-client';
 import App from './App';
 import { Provider } from './context/context';
 import './index.css';
 
 ReactDOM.render(
+  <SpeechlyProvider appId="247f43d2-fc40-490e-8b60-24945a944474" language="en-US">
   <Provider>
     <App />
-  </Provider>,
+  </Provider>
+  </SpeechlyProvider>,
   document.getElementById('root'))
 ;
