143634f1bf3882bdfc259fe8b18dd399b7b1bda1
AmrHE <amr.hassan.emam@gmail.com>
2021-04-17 22:44:18 +0200

    import speechly buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
 import Details from './Components/Details/Details';
 import Main from './Components/Main/Main';
 import useStyles from './appStyle';
@@ -15,15 +16,16 @@ function App() {
         <Grid item xs={12} sm={3}>
           <Details title="Income" />
         </Grid>
-
         <Grid item xs={12} sm={3}>
           <Main />
         </Grid>
-
         <Grid item xs={12} sm={3}>
           <Details title="Expense" />
         </Grid>
       </Grid>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+      </PushToTalkButtonContainer>
     </div>
   );
 }
