6ba8f1925517d8fac00f87f325e9da5631910cc8
AmrHE <amr.hassan.emam@gmail.com>
2021-04-21 04:38:00 +0200

    add localStorage

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -5,13 +5,14 @@ const contextReducer = (state, action) => {
   let transactions;
   switch (action.type) {
     case 'DELETE_TRANSACTION':
-      transactions = state.filter((t) => t.id !== action.payload)
+      transactions = state.filter((t) => t.id !== action.payload);
+      localStorage.setItem('transactions', JSON.stringify(transactions));
       return transactions;
-
-    case 'ADD_TRANSACTION':
-      transactions = [action.payload, ...state];
-
-      return transactions;
+      
+      case 'ADD_TRANSACTION':
+        transactions = [action.payload, ...state];
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+        return transactions;
 
     default:
       return state;
