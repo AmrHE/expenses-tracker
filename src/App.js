f51b6f74ba3a51636f0097d5bffd4b2230f35280
AmrHE <amr.hassan.emam@gmail.com>
2021-04-22 21:04:34 +0200

    add auto scroll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 import Details from './Components/Details/Details';
 import Main from './Components/Main/Main';
 import useStyles from './appStyle';
 
 
 function App() {
-
   const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null)
+
+  const executeScroll = () => main.current.scrollIntoView();
+
+  useEffect(() => {
+    if(speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
 
   return (
     <div>
       <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={3}>
-          <Details title="Expense" />
+        <Grid item xs={12} sm={3} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.last}>
+          <Details title="Expense"/>
         </Grid>
       <PushToTalkButtonContainer>
         <PushToTalkButton />
